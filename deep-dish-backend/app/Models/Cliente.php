384ace277d3004537f3d533e3cc716276e46432e
Eduardo Serra Pierre Vidal <eduardo.vidal@sempreceub.com>
2026-02-23 00:26:40 -0300

    feature: criado o modelo de clientes

--- deep-dish-backend/app/Models/Cliente.php
+++ deep-dish-backend/app/Models/Cliente.php
@@ -12,11 +12,6 @@ class Cliente extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -24,20 +19,11 @@ class Cliente extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+
     protected function casts(): array
     {
         return [
