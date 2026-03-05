b29f33baf22aac602ad5e60e8948911d277f27ac
joaogpereira <bilejg@gmail.com>
2026-03-05 19:27:48 -0300

    feat: busca restaurante

--- deep-dish-backend/app/Repositories/RestauranteRepository.php
+++ deep-dish-backend/app/Repositories/RestauranteRepository.php
@@ -14,9 +14,19 @@ class RestauranteRepository
             'email' => $data['email'],
             'cnpj' => $data['cnpj'],
             'tipo' => $data['tipo'],
-            'endereco' => $data['endereco'],
+            'logradouro' => $data['logradouro'],
+            'numero' => $data['numero'],
+            'complemento' => $data['complemento'] ?? null,
+            'bairro' => $data['bairro'],
+            'cidade' => $data['cidade'],
+            'estado' => strtoupper($data['estado']),
+            'cep' => $data['cep'],
+            'telefone' => $data['telefone'] ?? null,
+            'imagem_url' => $data['imagem_url'] ?? null,
+            'horario_funcionamento' => $data['horario_funcionamento'] ?? null,
+            'fila_ativa' => $data['fila_ativa'] ?? false,
             'tipo_usuario' => $data['tipo_usuario'],
-            'password' => $data['password'], 
+            'password' => $data['password'],
         ]);
     }
 }
