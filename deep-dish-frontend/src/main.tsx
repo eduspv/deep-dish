ba4ae84c76e715dc8a58c4754698fccea1ce69c9
joaogpereira <bilejg@gmail.com>
2026-04-17 09:22:29 -0300

    feat: scroll novo

--- deep-dish-frontend/src/main.tsx
+++ deep-dish-frontend/src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from "react-dom/client";
+import Lenis from "lenis";
 import App from "./App.tsx";
 import "./index.css";
+
+const lenis = new Lenis({ lerp: 0.08, smoothWheel: true });
+
+function raf(time: number) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
 
 createRoot(document.getElementById("root")!).render(<App />);
